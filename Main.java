42a5e690f925021b77f4c4760f8b453e73227c50
ilmiza fitrah <ilmizafitrah123@gmail.com>
2021-04-20 10:06:23 +0700

    update

--- Main.java
+++ Main.java
@@ -7,8 +7,8 @@
  */public class Main{
     
     public static void main(String args[]){
-        // menciptakan objek baru dari class Calculator
-        Calculator kalkul = new Calculator();
+        // menciptakan objek baru dari class kalkulator
+        kalkulator kalkul = new kalkulator();
         
         kalkul.name = "Operator ";
 
