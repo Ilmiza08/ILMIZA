1da5e67176c1401cc5632a3ea2129da5f27e88b8
ilmiza fitrah <ilmizafitrah123@gmail.com>
2021-04-20 09:49:33 +0700

    kalkulator

---
+++ kalkulator.java
@@ -0,0 +1,30 @@
+
+/**
+ * Write a description of class kalkulator here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class kalkulator{
+
+    String name;
+    
+    int a = 4;
+    int b = 5;
+    
+    int perkalian = a * b;
+    int pembagian = a / b;
+    int penjumlahan = a + b;
+    int pengurangan = a - b;
+    
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     
+    public int add(){
+        return x + y;
+    }*/
+}
