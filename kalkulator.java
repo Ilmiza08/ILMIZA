1e3119d2ac244e3b82b9906c316a2e7c84845fa7
ilmiza fitrah <ilmizafitrah123@gmail.com>
2021-04-20 09:52:48 +0700

    update kalkulator

--- kalkulator.java
+++ kalkulator.java
@@ -2,8 +2,8 @@
 /**
  * Write a description of class kalkulator here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Ilmiza Fitrah)
+ * @version (0.1-20.04)
  */
 public class kalkulator{
 
